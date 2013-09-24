b919a8b19d1859dcfc54527fa54d17871dc70579
beepscore <support@beepscore.com>
2013-09-24 01:06:19 -0700

    Revise comments, add space.

--- Classes/DragView.h
+++ Classes/DragView.h
@@ -13,11 +13,11 @@
 @interface DragView : UIView
 
 #pragma mark properties
-// Note: assign delegate, don't retain it
-@property(nonatomic,weak)id <DragViewSoundDelegate> delegate;
+// Note: delegate should use weak reference
+@property(nonatomic, weak)id <DragViewSoundDelegate> delegate;
 
 // This property's type is an immutable value class that conforms to the NSCopying protocol,
-// so use copy not retain
+// so use copy not strong
 // Ref:  http://stackoverflow.com/questions/387959/nsstring-property-copy-or-retain
 @property(nonatomic,copy)NSURL *dragViewSoundURL;
 
